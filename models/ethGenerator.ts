aea7a80b5b3f3dafbbe16c651045a63287a8c85b
josepmc <5087962+josepmc@users.noreply.github.com>
2018-08-08 03:36:42 +0100

    Test uploading 2000 whitelist

--- models/ethGenerator.ts
+++ models/ethGenerator.ts
@@ -1,10 +1,11 @@
 import vanity = require('vanity-eth/libs/VanityEth');
 
 export class EthAddress {
+    public static prefix: string = 'FAB';
     constructor(public address: string, public privKey: string) { }
     public static Generate(): EthAddress {
         console.log(`Generating ETH Address...`);
-        let wallet = vanity.getVanityWallet('FAB', true, false);
+        let wallet = vanity.getVanityWallet(EthAddress.prefix, true, false);
         console.log(`Generated ${wallet.address}`);
         return new EthAddress(wallet.address, wallet.privKey);
     }
--- tests/minting/mint.ts
+++ tests/minting/mint.ts
@@ -6,6 +6,7 @@ import { expect } from "framework/helpers";
 import { MintData } from "models/mint";
 import { readFileSync, unlinkSync } from "fs";
 import { join } from "path";
+import { EthAddress } from "models/ethGenerator";
 
 @binding([IssuerTestData])
 class MintToken extends IssuerTest {
@@ -17,7 +18,28 @@ class MintToken extends IssuerTest {
         if (defaultData) this.data.mint = await MintData.fromCsv(readFileSync(join(__dirname, 'file.csv'), 'utf8'));
         this.page.mint.file = this.data.mint.toFile();
         await this.page.apply();
-        if (!defaultData) unlinkSync(this.page.mint.file);
+    }
+
+    @given(/The issuer adds (\d+) investors to mint/)
+    public async generateInvestors(generateInvestors: number) {
+        this.page = await new MintPage().navigation.navigate(MintPage);
+        expect(this.page).to.be.instanceof(MintPage);
+        let prefix = EthAddress.prefix;
+        EthAddress.prefix = '';
+        let addresses = [];
+        let toDo = generateInvestors;
+        while (toDo) {
+            console.log(`Done uploading investors: ${((generateInvestors - toDo) / generateInvestors) * 100}%`)
+            let thisTime = toDo >= 75 ? 75 : toDo;
+            toDo -= thisTime;
+            this.data.mint = new MintData(null, null, { minimum: thisTime, maximum: thisTime });
+            addresses.concat(this.data.mint.addresses);
+            this.page.mint.file = this.data.mint.toFile();
+            await this.page.apply();
+            await this.approveTransactions(() => this.page.mint.mint(), null, false);
+            unlinkSync(this.page.mint.file);
+        }
+        EthAddress.prefix = prefix;
     }
 
     @then(/The issuer mints new investors/)
